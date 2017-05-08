abff50d874fa2700c058511b4916811759992571
はやみん <dev@hayam.in>
2017-05-08 20:47:45 +0900

    Fix git command for quiet

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ dotfiles="$HOME/.dotfiles"
 
 if [ ! -e $dotfiles/.git ]; then
     echo "not found dotfiles"
-    git clone https://github.com/rapwind/dotfiles.git $dotfiles
+    git clone https://github.com/rapwind/dotfiles.git $dotfiles --quiet
 else
     echo "dotfiles already exist"
-    cd $dotfiles && git pull
+    cd $dotfiles && git pull --quiet
 fi
 
-cd $dotfiles && git submodule init
-cd $dotfiles && git submodule update
+cd $dotfiles && git submodule init --quiet
+cd $dotfiles && git submodule update --quiet
 
 # dotfiles
 for f in $dotfiles/.??*; do
@@ -24,8 +24,8 @@ for f in $dotfiles/.??*; do
 done
 
 # prezto
-cd $dotfiles/.zprezto && git submodule init
-cd $dotfiles/.zprezto && git submodule update
+cd $dotfiles/.zprezto && git submodule init --quiet
+cd $dotfiles/.zprezto && git submodule update --quiet
 for rcfile in $dotfiles/.zprezto/runcoms/??*; do
     [[ "$(basename $rcfile)" == "README.md" ]] && continue
 
